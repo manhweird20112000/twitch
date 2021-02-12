c508f44b7e9f7a7e28b93fc6308c4dd0c859d6e7
manhweird20112000 <dinhvanmanh20112000@gmail.com>
2021-02-12 18:50:43 +0700

    Completed Effect form

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,6 +20,41 @@ let menuOption = document.querySelector('.main-menu__item--option');
 
 let subOption = document.querySelector('.sub-option');
 
+let headForms = document.querySelectorAll('.form-heading__title p');
+
+let tabsForm = document.querySelectorAll('.tabs-form__item');
+
+let formList = document.querySelectorAll('.form__item');
+
+let overlay = document.querySelector('.overlay');
+
+let btnRegister = document.querySelector('.btn--register');
+
+
+tabsForm.forEach((tabForm, index) => {
+
+    let lineTabForm = document.querySelector('.line-form');
+
+    tabForm.addEventListener('click', function() {
+        let activeTab = document.querySelector('.active-tab');
+        let activeHeading = document.querySelector('.show-title');
+        let activeForm = document.querySelector('.show-form');
+
+        lineTabForm.style.left = tabForm.offsetLeft + "px";
+        lineTabForm.style.width = tabForm.offsetWidth + "px";
+
+        activeHeading.classList.remove('show-title');
+        headForms[index].classList.add('show-title');
+
+        activeForm.classList.remove('show-form');
+        formList[index].classList.add('show-form');
+
+
+        activeTab.classList.remove('active-tab');
+        tabForm.classList.add('active-tab');
+    })
+})
+
 btnAccount.onclick = function() {
     optionAccount.classList.toggle("show");
 }
@@ -48,3 +83,12 @@ inputSearch.onblur = function(e) {
         searchBtn.style.cursor = 'pointer';
     }
 }
+
+function closeForm() {
+    let btnClose = document.querySelector('.close-form');
+
+    btnClose.addEventListener('click', function() {
+        overlay.style.display = 'none';
+    })
+}
+closeForm();
